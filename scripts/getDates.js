ac7980213f9f89e8bd7eb955841fdc132a9dfd23
Ezequiel Espada <e.h.espada@gmail.com>
2025-03-29 03:59:57 -0300

    dark mode on

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,3 +1,24 @@
 document.getElementById("year").textContent = new Date().getFullYear();
 
 document.getElementById("lastModified").textContent = "Last modified: " + document.lastModified;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
+
+const modeButton = document.querySelector("#mode");
+const body = document.body;
+
+modeButton.addEventListener("click", () => {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+        modeButton.textContent = "🔆";
+    } else {
+        modeButton.textContent = "🕶️";
+    }
+});
