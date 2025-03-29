e6d2779a3d958b2abe2f2794d6a3123c074c86f3
Ezequiel Espada <e.h.espada@gmail.com>
2025-03-29 15:51:27 -0300

    hamburguer menu

--- chamber/scripts/menu.js
+++ chamber/scripts/menu.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("lastModified").textContent = document.lastModified;
 });
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+	mainnav.classList.toggle('show');
+	hambutton.classList.toggle('show');
+});
